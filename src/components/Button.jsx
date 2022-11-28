fd368148be5f8fe183946b53d83df14d6a96a7d4
yaoming16 <pabloperezzz16@gmail.com>
2022-11-28 18:08:13 -0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,17 @@
 function Button( {letter, setScreenText, name, playSound, soundUrl} ) {
   
   return (
-    <button 
+    <div 
       className="w-1/3 bg-stone-600 m-2 text-white flex p-5 justify-center 
       cursor-pointer focus:border-0 transition-colors rounded-md font-bold select-none active:bg-slate-500 drum-pad shadow-xl"
-      onClick={(e) => {
+      onClick={() => {
         playSound(letter);
         setScreenText(name);     
-            }} 
+      }} 
       id={"Btn_" + letter}>
       {letter}
       <audio className="clip" id={letter} src={soundUrl} ></audio>
-    </button>
+    </div>
   )
 }
 
